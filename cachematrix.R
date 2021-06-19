7a8a2f3b0815bc987e29d18f9e6b2e9ad1eedc3d
Kevin Roche <kevinroche222@gmail.com>
2021-06-19 11:30:14 -0400

    Initial commit. Programming assignment two from week three of the RProgramming course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of a matrix while keeping the amount of
+## computational power used to a minimum.
 
-## Write a short comment describing this function
 
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL # Specify that object is absent
+        set <- function(y) { # Closure fn that access var's from parent fn
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x # Retrieves x
+        setInverse <- function(solveMatrix) inverse <<- solveMatrix # Sets value of inverse
+        getInverse <- function() inverse # Gets value of inverse
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then the cacheSolve should retrieve the inverse from
+## the cache.
 cacheSolve <- function(x, ...) {
+  
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        if(!is.null(inverse)) { # Check if inverse has already been calculated
+                print("Retrieving from cache")
+                return(inv)
+        }
+        matrixToInv <- x$get()
+        inverse <- solve(matrixToInv, ...)
+        x$setInverse(inverse)
+        inverse   
 }
+
+
+## Test function
+kevsMatrix <- makeCacheMatrix(matrix(1:4, 2, 2)) # Run fn and save in kevsMatrix var
+kevsMatrix$get() # Access matrix - success
+cacheSolve(kevsMatrix) # Solve inverse of matrix - success
+
+
+## Test function again
+kevsMatrix$set(matrix(c(2, 2, 1, 4), 2, 2)) # Set new matrix in kevsMatrix
+kevsMatrix$get() # Access matrix - success
+cacheSolve(kevsMatrix) # Solve inverse of matrix - success
